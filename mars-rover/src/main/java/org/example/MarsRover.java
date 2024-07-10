e06e65c61c7cb4b1b9afd93a94473c8f13058682
Gloal <alori.glo@gmail.com>
2024-07-10 15:36:23 +0100

    feat:separated left and right methods and able to calculate multiple left turns

--- mars-rover/src/main/java/org/example/MarsRover.java
+++ mars-rover/src/main/java/org/example/MarsRover.java
@@ -1,14 +1,45 @@
 package org.example;
 
 public class MarsRover {
+
+    char initialDirection = 'N';
     public String move(String s) {
+        int right_count = 0;
+        int left_count = 0;
         for(char c: s.toCharArray()) {
             if (c == 'R') {
-                return "00E";
+                right_count++;
             } else if (c == 'L'){
-                return "00W";
+                left_count++;
             }
         }
-        return null;
+        char finalDirection = right_count - left_count > 0? turnRight(right_count%4) : turnLeft(left_count%4);
+
+        return "00"+finalDirection;
     }
+
+    private static char turnLeft(int numOfTurns) {
+        char direction = 'N';
+        switch( numOfTurns){
+            case 1:
+                 direction = 'W';
+                 break;
+            case 2:
+                direction = 'S';
+                break;
+            case 3:
+                direction = 'E';
+                break;
+            default:
+
+        }
+        return direction;
+    }
+
+    public char turnRight(int numOfTurns){
+        return 'E';
+    }
+
+
 }
+
--- mars-rover/src/test/java/org/example/MarsRoverTest.java
+++ mars-rover/src/test/java/org/example/MarsRoverTest.java
@@ -1,10 +1,8 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,11 +16,14 @@ public class MarsRoverTest {
     @CsvSource({
             "00L, 00W",
             "00R, 00E",
+            "00LL, 00S",
+            "00LLLLLLL, 00E"
     })
     void turnsLeftOrRight_WhenGivenDirections(String directions, String expectedPosition){
         String actualPosition = rover.move(directions);
         assertEquals(expectedPosition, actualPosition);
     }
+
 
 
 
