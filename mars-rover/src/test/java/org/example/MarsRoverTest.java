4d6c688bcc72627ec005128968def831963ec409
Gloal <alori.glo@gmail.com>
2024-07-10 14:58:46 +0100

    fix:refactored tests to use parametized tests

--- mars-rover/src/test/java/org/example/MarsRoverTest.java
+++ mars-rover/src/test/java/org/example/MarsRoverTest.java
@@ -1,20 +1,29 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MarsRoverTest {
-
-    @Test
-    void turnsRight_whenGivenDirections(){
-        MarsRover rover = new MarsRover();
-        assertEquals("00E", rover.move("00R"));
+    private  MarsRover rover;
+    @BeforeEach
+    void SetUp(){
+        rover = new MarsRover();
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "00L, 00W",
+            "00R, 00E",
+    })
+    void turnsLeftOrRight_WhenGivenDirections(String directions, String expectedPosition){
+        String actualPosition = rover.move(directions);
+        assertEquals(expectedPosition, actualPosition);
     }
 
-    @Test
-    void turnsLeft_whenGivenDiretions(){
-        MarsRover rover = new MarsRover();
-        assertEquals("00W", rover.move("00L"));
-    }
+
+
 }
